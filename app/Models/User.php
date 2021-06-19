12ed483fd33f7af6389cf3440db22b8a0f3a5565
sendy452 <sendyivenyulian452@gmail.com>
2021-06-19 18:11:12 +0700

    Auth User API Passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $table = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -17,9 +20,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'nama',
         'email',
         'password',
+        'type',
+        'status',
     ];
 
     /**
